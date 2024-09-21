738a53966defebd07400768cf1d29d556079be68
mostafamaged99 <mostafaabdelmaged98@gmail.com>
2024-09-21 22:59:22 +0300

    second commit

--- venv/app.py
+++ venv/app.py
@@ -1,4 +1,4 @@
-from models import User
+from models import User, Group
 from sqlalchemy.orm import sessionmaker
 from db import engine
 
@@ -23,6 +23,74 @@ session = sessionmaker(bind=engine)()
 # session.commit()
 # print(updated_user)
 
-deleted_user = session.query(User).filter(User.id == 2).first()
-session.delete(deleted_user)
+# deleted_user = session.query(User).filter(User.id == 2).first()
+# session.delete(deleted_user)
+# session.commit()
+
+#user_1 = User(uname="Omar", password="123456")
+# session.add(user_1)
+# session.commit()
+# u1 = session.query(User).filter_by(id=1).first()
+
+# # profile_1 = Profile(fname= "omar",lname = "ali", user = u1)
+# # session.add(profile_1)
+# # session.commit()
+# # p1 = session.query(Profile).filter_by(id=1).first()
+# print(u1.profile.fname)
+# u1.profile.fname = "sara"
+# u1.profile.lname = "omr"
+# session.commit()
+# session.delete(u1)
+# session.commit()
+
+# user_1 = User(uname="Omar")
+# session.add(user_1)
+# session.commit()
+# u1 = session.query(User).filter_by(id=1).first()
+# # print(u1)
+# # posts = [
+# #     {
+# #         "title": "post 1",
+# #         "content": "content 1"
+# #     },
+# #     {
+# #         "title": "post 2",
+# #         "content": "content 2"
+# #     },
+# #     {
+# #         "title": "post 3",
+# #         "content": "content 3"
+# #     }
+# # ]
+# # for post_data in posts:
+# #     post = Post(title=post_data["title"], content=post_data["content"], user=u1)
+# #     session.add(post)
+# #     session.commit()
+# u1.posts[0].title = "new title 1"
+# session.commit()
+# session.delete(u1)
+# session.commit()
+
+# session.add_all([
+#     User(uname="Omar"),
+#     User(uname="Ahmed"),
+#     User(uname="Sara"),
+#     Group(g_name= "python"),
+#     Group(g_name= "javascript"),
+#     Group(g_name= "C++")
+# ])
+# session.commit()
+u1 = session.query(User).filter_by(id=1).first()
+u2 = session.query(User).filter_by(id=2).first()
+u3 = session.query(User).filter_by(id=3).first()
+g1 = session.query(Group).filter_by(id=1).first()
+g2 = session.query(Group).filter_by(id=2).first()
+g3 = session.query(Group).filter_by(id=3).first()
+u1.groups.append(g1)
+u1.groups.append(g2)
+u1.groups.append(g3)
+u2.groups.append(g3)
+u3.groups.append(g2)
+print(u1.groups)
 session.commit()
+
--- venv/models.py
+++ venv/models.py
@@ -1,12 +1,62 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from db import engine, Base
+
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     uname = Column(String(20), nullable=False, unique=True)
+#     password = Column(String(20), nullable=False)
+#     profile = relationship("Profile", backref="user", uselist=False, cascade="all, delete, save-update")
+#     def __repr__(self) -> str:  
+#         return f"Hello, {self.uname}!)"
+    
+# class Profile(Base):
+#     __tablename__ = "profiles"
+#     id = Column(Integer, primary_key=True)
+#     fname = Column(String(20))
+#     lname = Column(String(20))
+#     user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE", onupdate = "CASCADE"))
+#     def __repr__(self) -> str:
+#         return f"Hello, {self.fname} {self.lname}!"
+
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     uname = Column(String(30), nullable=False, unique=True)
+#     posts = relationship("Post", back_populates="user", cascade="all, delete, save-update")
+#     def __repr__(self) -> str:
+#         return f"Hello, {self.uname}!"
+    
+# class Post(Base):
+#     __tablename__ = 'posts'
+#     id = Column(Integer, primary_key=True)
+#     title = Column(String(50), nullable=False)
+#     content = Column(String(255), nullable=False)
+#     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE", onupdate = "CASCADE"))
+#     user = relationship("User", back_populates="posts")
+
+group_user = Table(
+    "group_users",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("group_id", Integer, ForeignKey("groups.id"), primary_key=True),
+)
 
 class User(Base):
     __tablename__ = 'users'
-    id= Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-
-    def __repr__(self):
-        return f"<User(id={self.id}, name={self.name})>"
+    id = Column(Integer, primary_key=True)
+    uname = Column(String(30), nullable=False, unique=True)
+    groups = relationship("Group", secondary=group_user, back_populates="users")
+    def __repr__(self) -> str:
+        return f"Hello, {self.uname}!"
+    
+class Group(Base): 
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    g_name = Column(String(30), nullable=False)
+    users = relationship("User", secondary=group_user, back_populates= "groups")
+    def __repr__(self) -> str:
+        return f"Hello, {self.g_name}!"
 
 Base.metadata.create_all(bind = engine)
